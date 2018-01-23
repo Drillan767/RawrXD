449da1744ba7a1c89d16793ecd6eae9fff6463c5
Drillan767 <jd.levarato@gmail.com>
2018-01-23 10:33:46 +0100

    Component refactoring completed - Bootstrap removed from app/assets/stylesheet, not yet added to app/javascript - Created a const where all my elements are called when they need to be - Created a separated function that returns the action name. No problem front, we'll for the admin part.

---
+++ app/javascript/packs/home/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class homeIndex extends React.Component {
+    render() {
+        return(
+            <h1>Bonjour les zamiiiii</h1>
+        )
+    }
+}
